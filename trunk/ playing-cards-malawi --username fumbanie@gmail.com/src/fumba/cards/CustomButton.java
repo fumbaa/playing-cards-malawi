3f79a8f8c0e0a303a18d08aed8b8ddc04163beda
fumbanie@gmail.com <fumbanie@gmail.com@5eac560f-010e-8505-3425-f1609816bd43>
2011-11-10 19:33:25 +0000

    CLEANED VERSION FOR SDK R15.

---
+++ trunk/ playing-cards-malawi --username fumbanie@gmail.com/src/fumba/cards/CustomButton.java
@@ -0,0 +1,35 @@
+package fumba.cards;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+
+/**
+ * Customised menu buttons.
+ * <p><i>Copyright (c) 1998, 2011 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution.</i></p>
+ * 
+ * @author Fumbani Chibaka, 
+ * @version 1.0, 10/28/2011
+ * @see <a href="http:chibaka.com">Fumba Game Lab</a>
+ */
+
+
+public class CustomButton extends FGLGraphic {
+
+
+	CustomButton(Context context, String name, int resourceID)
+	{
+		super(context, name, resourceID);
+	}
+
+	@Override
+	protected void extractBitmap() {
+		BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inJustDecodeBounds = true;
+        this.img = BitmapFactory.decodeResource(context.getResources(), resourceID); 
+        this.width = this.getBitmap().getWidth();
+        this.height = this.getBitmap().getHeight();
+	}
+
+}
