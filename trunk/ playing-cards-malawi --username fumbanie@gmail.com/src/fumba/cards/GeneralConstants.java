0bfcf4ea5ab641798ab7336d5133fd1953426f27
fumbanie@gmail.com <fumbanie@gmail.com@5eac560f-010e-8505-3425-f1609816bd43>
2011-12-23 03:41:22 +0000

    General Constants used during gameplay

---
+++ trunk/ playing-cards-malawi --username fumbanie@gmail.com/src/fumba/cards/GeneralConstants.java
@@ -0,0 +1,17 @@
+package fumba.cards;
+
+public class GeneralConstants {
+
+	/** Chichewa Language **/
+	public static final String CHICHEWA = "Chichewa";
+	
+	/** Tumbuka Language **/
+	public static final String TUMBUKA = "Tumbuka";
+	
+	/** English Language **/
+	public static final String ENGLISH = "English";
+
+	/** Unknown Language Error Message **/
+	public static final String LN_UNKNOWN = "Error: Unknown Language";
+
+}
