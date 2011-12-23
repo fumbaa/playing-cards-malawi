38cd0841b9289cef0c4c5fd064094a70042bbab2
fumbanie@gmail.com <fumbanie@gmail.com@5eac560f-010e-8505-3425-f1609816bd43>
2011-12-23 03:35:59 +0000

    FGL Message - CHICHEWA, TUMBUKA and ENGLISH

---  playing-cards-malawi --username fumbanie@gmail.com/src/fumba/cards/FGLMessage.java
+++  playing-cards-malawi --username fumbanie@gmail.com/src/fumba/cards/FGLMessage.java
@@ -4,11 +4,20 @@ import org.apache.commons.lang3.StringUtils;
 
 public class FGLMessage {
 
-	
+	/**
+	 * String message produced when user tries to make an invalid move
+	 * @param language Selected user language
+	 * @return String an instruction in the selected language
+	 */
 	public static String getMoveNotAllowed(String language) {
-		if ( StringUtils.equals("Hie", "Hallo"))
-		return "hallo";
-		return language;
+		if (StringUtils.equals(GeneralConstants.CHICHEWA, language)) {
+			return MessageConstants.CH_NOT_ALLOWED;
+		} else if (StringUtils.equals(GeneralConstants.ENGLISH, language)) {
+			return MessageConstants.EN_NOT_ALLOWED;
+		} else if (StringUtils.equals(GeneralConstants.TUMBUKA, language)) {
+			return MessageConstants.TM_NOT_ALLOWED;
+		}
+		return GeneralConstants.LN_UNKNOWN;
 	}
 
 }
