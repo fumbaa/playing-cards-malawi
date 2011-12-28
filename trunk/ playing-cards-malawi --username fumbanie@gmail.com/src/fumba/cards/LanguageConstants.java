d75553f7f47a5b5444060e3c83ba32dff9c4a71a
fumbanie@gmail.com <fumbanie@gmail.com@5eac560f-010e-8505-3425-f1609816bd43>
2011-12-28 15:11:22 +0000

    Changed the FGLMessage to LanguageConstants

---
+++ trunk/ playing-cards-malawi --username fumbanie@gmail.com/src/fumba/cards/LanguageConstants.java
@@ -0,0 +1,15 @@
+package fumba.cards;
+
+public interface LanguageConstants {
+
+	/** ENGLISH - Player not allowed to make move **/
+	public final static String EN_NOT_ALLOWED = "You are not allowed to make this move...";
+
+	/** CHICHEWA - Player not allowed to make move **/
+	public final static String CH_NOT_ALLOWED = "Simungamenye muvu imeneyi panthawi ino...";
+
+	/** TUMBUKA- Player not allowed to make move **/
+	public static final String TM_NOT_ALLOWED = "Ivo mukuchita nvyakulolezeka yayi";
+	
+
+}
