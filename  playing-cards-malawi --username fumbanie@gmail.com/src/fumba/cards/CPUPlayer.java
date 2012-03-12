c29aa0f1607e4cac6652eb776cce926d02a08fc2
fumbanie@gmail.com <fumbanie@gmail.com@5eac560f-010e-8505-3425-f1609816bd43>
2012-03-12 15:01:09 +0000

    Added CPUPlayer.java

---
+++  playing-cards-malawi --username fumbanie@gmail.com/src/fumba/cards/CPUPlayer.java
@@ -0,0 +1,76 @@
+package fumba.cards;
+
+import java.util.List;
+
+public class CPUPlayer implements Player {
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Boolean isHuman() {
+		return false;
+	}
+
+	public void addCard(Card card) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void recalculatePositions() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Card> getCardsInHand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void returnCards() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void bringCardToFront(Card card) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Card getCard(String activeCard) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void playCard(Move move) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void pickCard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int countCardsInHands() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Move getCurrentMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setCurrentMove(Move move) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean hasValidMoves() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
